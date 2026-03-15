faa226035a1b6e371c81f0bbb3487820ade46611
Linus Riches <19137473@hlnsc.ac.uk>
2026-03-15 21:04:06 +0000

    Simple formatting changes

--- main.js
+++ main.js
@@ -1,91 +1,95 @@
 //INITIAL LOAD, CANVAS BACKGROUND CONSTS
 
-window.addEventListener('load', function()
-	{const canvas = document.getElementById('canvas1');
-	const bgimg = this.document.getElementById('bgimg');
-	const context = canvas.getContext('2d');
+window.addEventListener('load', function () {
+	const gamespace = document.getElementById('gamespace');
+	const brick_bg = this.document.getElementById('brick_bg');
+	const context = gamespace.getContext('2d');
 
-//GAMETITLE
+	//GAMETITLE (not sure if I need it so i replaced it with assets/null)
 
 	const titleImage = new Image();
-	titleImage.src = 'assets/title.png';
-	const GameTitle = {x: 450, y: 0, width: 500, height: 80,};
+	titleImage.src = 'assets/null.png';
+	const GameTitle = { x: 450, y: 0, width: 500, height: 80, };
 
-//CHICKEN OBJECT
+	//CHICKEN OBJECT
 
 	const chickenImage = new Image();
 	chickenImage.src = 'assets/chicken.png';
-	const Chicken = {x: 350, y: 350, width: 64, height: 64,};
+	const Chicken = { x: 350, y: 350, width: 64, height: 64, };
 
-//CANVAS AND BACKGROUND DIMENSIONS
+	//CANVAS AND BACKGROUND DIMENSIONS
 
-	canvas.width = 1440
-	canvas.height = 960
-	bgimg.width =  1800
-	bgimg.height = 1200
+	gamespace.width = 1300
+	gamespace.height = 700
+	brick_bg.width = 1800
+	brick_bg.height = 1200
 
-//PLAYER OBJECT
+	//PLAYER OBJECT
 
 	var playerImage = new Image();
-		playerImage.src = 'assets/goat.png';
-	var Player = {x: 200, y: 150, width: 64, height: 64, speed: 3,};
+	playerImage.src = 'assets/goat.png';
+	var Player = { x: 200, y: 150, width: 64, height: 64, speed: 3, gravity: 3,};
 
-// KEY CONTROL EVENT LISTENERS
+	// KEY CONTROL EVENT LISTENERS
 
 	const keys = {};
-		window.addEventListener('keydown', function(e) {e.preventDefault(); keys[e.key] = true;});
-		window.addEventListener('keyup', function(e) {e.preventDefault(); keys[e.key] = false;});
+	window.addEventListener('keydown', function (e) { e.preventDefault(); keys[e.key] = true; });
+	window.addEventListener('keyup', function (e) { e.preventDefault(); keys[e.key] = false; });
 
-// MUSIC & SOUND FUNCTION
+	// MUSIC & SOUND FUNCTION
 
 	var SoundCollide = new Audio('assets/bingo.wav');
-		SoundCollide.loop = false;
-	var MusicChilderness = new Audio('assets/bingo.wav');
-		MusicChilderness.loop = false;
+	SoundCollide.loop = false;
+	var MusicChilderness = new Audio('assets/childerness.wav');
+	MusicChilderness.loop = true;
 
-// COLLISION FUNCTION
+	// COLLISION FUNCTION
 
-	function collisionDetecting(Player, Chicken)
-		{if (Player.x + Player.width >= Chicken.x && Player.x <= Chicken.x + Chicken.width && Player.y + Player.height >= Chicken.y && Player.y <= Chicken.y + Chicken.height)
-			{console.log("collision yaaaay"); SoundCollide.play();}};
+	function collisionDetecting(Player, Chicken) {
+		if (Player.x + Player.width >= Chicken.x && Player.x <= Chicken.x + Chicken.width && Player.y + Player.height >= Chicken.y && Player.y <= Chicken.y + Chicken.height) { console.log("collision yaaaay"); SoundCollide.play(); }
+	};
 
-// BOUNDING FUNCTION FOR CEILING, WALLS AND FLOOR
+	// BOUNDING FUNCTION FOR CEILING, WALLS AND FLOOR
 
-	function boundingBox(Player)
-		{if (Player.y < 3) {Player.y = 3}
-		else if (Player.y > 893) {Player.y = 893}
-		else if (Player.x < -5) {Player.x = -5}
-		else if (Player.x > 1385) {Player.x = 1385}};
+	function boundingBox(Player) {
+		if (Player.y < 3) { Player.y = 3 }
+		else if (Player.y > 893) { Player.y = 893 }
+		else if (Player.x < -5) { Player.x = -5 }
+		else if (Player.x > 1385) { Player.x = 1385 }
+	};
 
-// 🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨
+	// 🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨GAMELOOP🟨
+	function gameloop() {
+		context.clearRect(0, 0, gamespace.width, gamespace.height); context.imageSmoothingEnabled = false;
 
-	function gameloop()
-		{context.clearRect(0, 0, canvas.width, canvas.height); context.imageSmoothingEnabled = false;
-
-// KEY CHECK AND BINDINGS
+		// KEY CHECK AND BINDINGS
 
 		const speed = keys['c'] ? Player.speed + 1.5 : Player.speed;
-			if (keys['w']) Player.y -= speed;
-			if (keys['s']) Player.y += speed;
-			if (keys['a']) Player.x -= speed;
-			if (keys['d']) Player.x += speed;
+		if (keys['w']) Player.y -= speed;
+		if (keys['s']) Player.y += speed;
+		if (keys['a']) Player.x -= speed;
+		if (keys['d']) Player.x += speed;
 
-// DRAW OBJECTS (PLAYER AND ROCK)
+		// DRAW OBJECTS (PLAYER AND ROCK)
 
 		context.drawImage(playerImage, Player.x, Player.y, Player.width, Player.height);
 		context.drawImage(titleImage, GameTitle.x, GameTitle.y, GameTitle.width, GameTitle.height);
 		context.drawImage(chickenImage, Chicken.x, Chicken.y, Chicken.width, Chicken.height);
 
-// CALLING COLLISION AND BOUNDING FUNCTION
+		// CALLING COLLISION AND BOUNDING FUNCTION
 
 		collisionDetecting(Player, Chicken);
 		boundingBox(Player);
 
-// NEXT FRAME
+		// NEXT FRAME
 
-		requestAnimationFrame(gameloop);};
-		playerImage.onload = gameloop})
+		requestAnimationFrame(gameloop);
+		MusicChilderness.play();
+	};
+
+	playerImage.onload = gameloop
+})
 
 
 
-		// using css is not working well for the backgrounds so maybe switch to making them in js?????
+// using css is not working well for the backgrounds so maybe switch to making them in js?????
